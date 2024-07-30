6e612109d91415b8e1ebb7ba58b7b1f1d04d254c
Geismark <geismark@btinternet.com>
2024-07-30 12:39:03 +0100

    utils init; added words_util

---
+++ src/utils/words_util.py
@@ -0,0 +1,10 @@
+def number_to_words(num):
+    num = int(num)
+    if 0 <= num <= 25:
+        return chr(num + 65)
+    elif 65 <= num <= 90:
+        return chr(num)
+    elif 97 <= num <= 122:
+        return chr(num - 32)
+    else:
+        raise ValueError(f"Invalid number: {num}")
